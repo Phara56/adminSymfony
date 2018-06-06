952d488c03f26518ed7b73c973a9e64b235b7381
Phara56 <guillaumecuriel95@gmail.com>
2018-06-06 19:53:00 +0200

    'propre'

--- web/js/custom.js
+++
@@ -1,5 +0,0 @@
-$('#tooltipBottom').tooltip('show');
-function closeNotif(){
-    var notif = document.getElementsByClassName("alert alert-primary notif-username");
-    notif[0].style.display = "none";
-}
